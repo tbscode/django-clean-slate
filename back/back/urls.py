0d3a975f25ae2e00125fd4538539e1d360782c18
tim <tim@timschupp.de>
2022-10-29 01:19:07 +0200

    integrated drf-spectacular

--- back/back/urls.py
+++ back/back/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 """
 We are adding all app urls under `'/'` their paths should be set under `<app>/urls.py`
@@ -9,4 +12,4 @@ Admin paths registered last
 urlpatterns = [
     path('', include('management.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- back/management/urls.py
+++ back/management/urls.py
@@ -1,9 +1,19 @@
 from . import api
 from django.urls import path
+from django.conf import settings
+if settings.BUILD_TYPE in ['development', 'staging']:
+    from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 VERSION = 1
 
 urlpatterns = [
     path(f"api/v{VERSION}/user_data/", api.user_app_data),
     path(f"api/v{VERSION}/register/", api.register),
+    *([
+        path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+        path('api/schema/swagger-ui/',
+             SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+        path('api/schema/redoc/',
+             SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    ] if settings.BUILD_TYPE in ['development', 'staging'] else [])
 ]
