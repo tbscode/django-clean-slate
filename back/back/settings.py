6373d6fefbb2b4190022d5b6118c8d529150e5f6
tim <tim@timschupp.de>
2022-10-29 05:08:09 +0200

    fixed template inclusion

--- back/back/settings.py
+++ back/back/settings.py
@@ -48,7 +48,9 @@ ROOT_URLCONF = 'back.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "template/")
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
